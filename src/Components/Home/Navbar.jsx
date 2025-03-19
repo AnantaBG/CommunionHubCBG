87c0c2d9ceb3e3eb8251c69b45e0e69940db61a9
AnantaBG <anantabanikofficial@gmail.com>
2025-03-19 19:47:27 +0600

    Navbar with Logo and Events Page

---
+++ src/Components/Home/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Navbar } from "flowbite-react";
+
+const NavBar = () => {
+    return (
+        <Navbar className='sticky top-0 z-50' fluid rounded>
+      <Navbar.Brand href="https://flowbite-react.com">
+        <img src="/src/assets/C.png" className='max-h-10 max-w-10' srcset="" />
+        <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">CommunionHUB</span>
+      </Navbar.Brand>
+      <Navbar.Toggle />
+      <Navbar.Collapse>
+        <Navbar.Link href="#" active>
+          Home
+        </Navbar.Link>
+        <Navbar.Link href="#">
+          Events
+        </Navbar.Link>
+      </Navbar.Collapse>
+    </Navbar>
+    );
+};
+
+export default NavBar;
