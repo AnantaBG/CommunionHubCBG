e024073d5bc11315fe88abe4a4868b03c6c0c666
AnantaBG <anantabanikofficial@gmail.com>
2025-03-19 21:28:25 +0600

    CTA button

--- src/Components/Home/Hero.jsx
+++ src/Components/Home/Hero.jsx
@@ -3,33 +3,34 @@ import Img1 from "../../assets/banner1.jpg"
 import Img2 from "../../assets/banner2.jpg"
 import Img3 from "../../assets/banner3.jpg"
 import Img4 from "../../assets/banner4.jpg"
-import { Carousel } from "flowbite-react";
+import { Button, Carousel } from "flowbite-react";
 const Hero = () => {
     return (
         <div className="mt-10 w-11/12 mx-auto">
-            <div className="h-96">
-      <Carousel>
+    <div className="lg:h-96 md:h-80 h-60">
+      <Carousel className="rounded-full">
       <div className="flex h-full items-center justify-center bg-gray-400 dark:bg-gray-700 dark:text-white">
         <div>
-        <img src={Img1} alt="" />
+        <img  className="lg:h-96 md:h-80 h-60" src={Img1} alt="" />
         </div>
         </div>
         <div className="flex h-full items-center justify-center bg-gray-400 dark:bg-gray-700 dark:text-white">
         <div>
-        <img src={Img2} alt="" />
+        <img  className="lg:h-96 md:h-80 h-60" src={Img2} alt="" />
         </div>
         </div>
         <div className="flex h-full items-center justify-center bg-gray-400 dark:bg-gray-700 dark:text-white">
         <div>
-        <img src={Img3} alt="" />
+        <img  className="lg:h-96 md:h-80 h-60" src={Img3} alt="" />
         </div>
         </div>
-        <div className="flex h-full items-center justify-center bg-gray-400 dark:bg-gray-700 dark:text-white">
+        <div  className="flex h-full items-center justify-center bg-gray-400 dark:bg-gray-700 dark:text-white">
         <div>
-        <img src={Img4} alt="" />
+        <img  className="lg:h-96 md:h-80 h-60" src={Img4} alt="" />
         </div>
         </div>
       </Carousel>
+      <Button href="events" className="flex justify-center mx-auto mt-5">Explore Events</Button>
     </div>
         </div>
     );
